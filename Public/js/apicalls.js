b20a22dbc0bdb12217f818285088a4c74dfadf1e
Samantha Scott <Samanthascott1992@gmail.com>
2020-05-05 14:42:44 -0600

    google maps api works!

---
+++ Public/js/apicalls.js
@@ -0,0 +1,17 @@
+// $(document).ready(function () {
+
+
+//     // Create the script tag, set the appropriate attributes
+//     var query = document.createElement("div");
+//     query.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBHEIDB-6P3pJ9yo71rY726T8kErrUENH0&callback=initMap";
+//     query.defer = true;
+//     query.async = true;
+
+//     // Attach your callback function to the `window` object
+//     window.initMap = function() {
+//     // JS API is loaded and available
+
+//     };
+
+//     document.body.append(query);
+// });
